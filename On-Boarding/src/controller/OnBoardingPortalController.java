47518679df19594ff45b99abec3695cf17940544
sg43 <swati.goel@snapdeal.com>
2015-08-13 14:55:49 +0530

    controller with loginUser and logout done

--- On-Boarding/src/controller/OnBoardingPortalController.java
+++ On-Boarding/src/controller/OnBoardingPortalController.java
@@ -6,11 +6,43 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
-import Entities.Admin;
-import Entities.Employee;
-import Entities.HR;
+import Entities.*;
 
 public interface OnBoardingPortalController  {
 
 	//TODO:add methods after changing signature
+	
+	public ModelAndView loginUser(HttpServletRequest request,
+			HttpServletResponse response, @ModelAttribute("") User user);
+
+	public ModelAndView logout(HttpServletRequest request,
+			HttpServletResponse response);
+
+	public ModelAndView addHR(HttpServletRequest request,
+			HttpServletResponse response, @ModelAttribute("") HR hr);
+
+	public ModelAndView removeHR(HttpServletRequest request,
+			HttpServletResponse response, @ModelAttribute("") HR hr);
+
+	public ModelAndView generatePassword(HttpServletRequest request,
+			HttpServletResponse response, @ModelAttribute("") HR hr);
+
+	public ModelAndView createGroup(HttpServletRequest request,
+			HttpServletResponse response, @ModelAttribute("") Group group);
+
+	public ModelAndView removeGroup(HttpServletRequest request,
+			HttpServletResponse response, @ModelAttribute("") Group group);
+
+	public ModelAndView createPost(HttpServletRequest request,
+			HttpServletResponse response, @ModelAttribute("") Message message);
+
+	public ModelAndView removePost(HttpServletRequest request,
+			HttpServletResponse response, @ModelAttribute("") Message message);
+
+	public ModelAndView updateProfileOfEmployee(HttpServletRequest request,
+			HttpServletResponse response, @ModelAttribute("") Employee employee);
+
+	public ModelAndView updateProfileOfHR(HttpServletRequest request,
+			HttpServletResponse response, @ModelAttribute("") HR hr);
+
 }
